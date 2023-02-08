5952c11c6d441677de00a802502067ed967c3c10
Thomas Abdalla <thomashenryabdalla@gmail.com>
2023-02-07 19:17:32 -0800

    Initialized the HomePageActivity.kt file and its corresponding xml (activity_homepage.xml).

---
+++ app/src/main/java/com/example/cs4530_mobileapp/HomePageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cs4530_mobileapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homepage)
+    }
+}
